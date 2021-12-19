cfdec2f9f0c08613403751206f05e9170b987e41
LeoJiang1997 <1075001194@qq.com>
2021-12-19 15:23:16 +0000

    26

--- 数组/26删除有序数组中的重复项.ts
+++ 数组/26删除有序数组中的重复项.ts
@@ -1,14 +1,22 @@
 function removeDuplicates(nums: number[]): number {
-    const sortNum = nums.sort();
-    sortNum.forEach((value,index) => {
-        const b = index + 1;
-        
-        if(value === nums[index+1]){
-            nums.splice(index,1);
+    const len = nums.length;
+    if (len == 0) return 0;
+    let slow = 0, fast = 1;
+    while (fast < len) {
+    // 双指针查找一样的元素，假如两个指针指的元素不一样
+    // 慢指针往前一格,即指到了重复元素中后面的那个
+    // 把重复的值用后面不重复的值覆盖掉        
+    if (nums[fast] != nums[slow]) {
+            // 使慢指针下一个值指向快指针值
+            slow++;
+            nums[slow] = nums[fast];
         }
-    })
-  return sortNum.length;
+        fast++;
+    }
+    // slow后面都是重复元素,
+    //所以这里返回的数组只需要返回到return的length
+    return slow + 1;
 };
 
- console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
+console.log(removeDuplicates([1,1,2]));
  
