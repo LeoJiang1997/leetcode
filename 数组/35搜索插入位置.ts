1a635d80e46033f271abe760a863bfd05a8ae769
LeoJiang1997 <1075001194@qq.com>
2021-12-21 15:39:05 +0000

    35

--- 数组/35搜索插入位置.ts
+++ 数组/35搜索插入位置.ts
@@ -1,18 +1,18 @@
 function searchInsert(nums: number[], target: number): number {
   let left = 0,right = nums.length;
   while(left < right){
+      //为了防止数字溢出，因为left和right肯定不超过整数最大值，但left+right就不一定了
       const middle = left + Math.floor((right - left)/2);
       var mid = Math.floor((right - left)/2);
       if(nums[middle] > target){
           right = middle; //目标值在左边，往左边查找
       }else if(nums[middle] > target){
           left = middle; //目标值在右边，往右边查找
-      } else {
-          console.log(mid,middle);
+      } else if(nums[middle] === target){
           return middle;
       }
   }
-  console.log(mid);
+  // 如果没找到就返回插入下标
   return right;
 };
 
