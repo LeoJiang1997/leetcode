950cd53b6635a70545aea6804c261b47dedc4278
LeoJiang1997 <1075001194@qq.com>
2022-01-18 00:49:16 +0000

    1.18

--- 排序/bubbleSort.ts
+++ 排序/bubbleSort.ts
@@ -1,11 +1,13 @@
-// function randArray(len, min, max) {
-//   return Array.from({length:len}, v => Math.floor(Math.random()*(max-min))+min);
-// }
+function randArray(len, min, max) {
+  return Array.from({length:len}, v => Math.floor(Math.random()*(max-min))+min);
+}
 
   let arrBubble: number[];
+  // 冒泡排序，谁大谁交换（冒泡）
   function bubbleSort(arr: number[]): number[]{
-    for (let i = arr.length-1; i > 0; i--){
-      for(let j = 0; j < arr.length; j++){
+    let len = arr.length;
+    for(let i = len - 1;i > 0;i--){
+      for(let j = 0;j < i; j++){
         if(arr[j] > arr[j+1]){
           let temp = arr[j];
           arr[j] = arr[j+1];
@@ -13,6 +15,15 @@
         }
       }
     }
+    // for (let i = arr.length-1; i > 0; i--){
+    //   for(let j = 0; j < arr.length; j++){
+    //     if(arr[j] > arr[j+1]){
+          // let temp = arr[j];
+          // arr[j] = arr[j+1];
+          // arr[j+1] = temp;
+    //     }
+    //   }
+    // }
     return arr;
   }
   
--- 排序/selectSort.ts
+++ 排序/selectSort.ts
@@ -1,9 +1,10 @@
 const arr = [9, 1, 4, 7, 2, 0, 8];
+// 选择排序选最小
 function selectSort(array: number[]): number[] {
   let length = array.length;
   for (let i = 0; i < length - 1; i++) {
     let minPro = i;
-    // 把一个数组拆成两半，j为另一半，从里面寻找那个最小值放进已经排完的数组中
+    // 从未排序数组里寻找那个最小值放进已经排完的数组中
     for (let j = i + 1; j < length; j++) {
       minPro = array[minPro] < array[j] ? minPro : j;
     }
@@ -13,4 +14,6 @@ function selectSort(array: number[]): number[] {
   }
   return array;
 }
+
+
 console.log(selectSort(arr));
